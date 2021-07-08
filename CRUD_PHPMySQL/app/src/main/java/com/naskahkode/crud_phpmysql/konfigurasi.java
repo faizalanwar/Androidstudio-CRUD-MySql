d5f41062434f6b508ee58a027d2a458af226ece2
faizalanwar <fzlanwr@gmail.com>
2021-07-08 22:06:08 +0700

    wushh

---
+++ CRUD_PHPMySQL/app/src/main/java/com/naskahkode/crud_phpmysql/konfigurasi.java
@@ -0,0 +1,33 @@
+package com.naskahkode.crud_phpmysql;
+
+
+public class konfigurasi {
+//    public static final String URL_ADD="http://android.unggasid.com/mhs/tambahMhs.php";
+//    public static final String URL_GET_ALL = "http://android.unggasid.com/mhs/tampilSemuaMhs.php";
+//    public static final String URL_GET_EMP ="http://android.unggasid.com/mhs/tampilMhs.php?id=";
+//    public static final String URL_UPDATE_EMP = "http://android.unggasid.com/mhs/updateMhs.php";
+//    public static final String URL_DELETE_EMP = "http://android.unggasid.com/mhs/hapusMhs.php?id=";
+
+
+    public static final String URL_ADD="http://192.168.43.11/pab/tambahMhs.php";
+    public static final String URL_GET_ALL = "http://192.168.43.11/pab/tampilSemuaMhs.php";
+    public static final String URL_GET_EMP ="http://192.168.43.11/pab/tampilMhs.php?id=";
+    public static final String URL_UPDATE_EMP = "http://192.168.43.11/pab/updateMhs.php";
+    public static final String URL_DELETE_EMP = "http://192.168.43.11/pab/hapusMhs.php?id=";
+
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "name";
+    public static final String KEY_MHS_NRP = "nrp";
+    public static final String KEY_MHS_KELAS = "kelas";
+    public static final String KEY_MHS_BLOG = "blog";
+
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "name";
+    public static final String TAG_NRP = "nrp";
+    public static final String TAG_KELAS = "kelas";
+    public static final String TAG_BLOG = "blog";
+
+    public static final String MHS_ID = "mhs_id";
+}
